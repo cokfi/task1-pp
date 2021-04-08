68300d267ea5238ef7e4700114aed6ccc28f2512
Kfir Cohen <cokfi@post.bgu.ac.il>
2021-04-08 11:37:43 +0300

    modified while into for loop

--- Project1/main.c
+++ Project1/main.c
@@ -18,12 +18,12 @@ char** argv;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     d_inside = 0;
-    d_total = 0;
+    d_total = 8064000000;
+    d_per_core = d_total / (long long int)size;
     srand(time(NULL) + rank);
-    pi = 0.0;
+    pi = 0;
     done = 0;
-    do {
-        for (i = 0; i < 100000; i++) {
+        for (i = 0; i < d_per_core; i++) {
             x = (double)rand() / RAND_MAX;
             y = (double)rand() / RAND_MAX;
             rad = x * x + y * y;
@@ -31,23 +31,18 @@ char** argv;
             //printf("di is %d \n " ,d_inside);
         }
         MPI_Reduce(&d_inside, &inside_tot, 1, MPI_LONG_LONG_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-            if (rank == 0) {
-                //printf("in_tot is %lli \n " ,inside_tot);
-                d_total = d_total + (long long int)size * 100000;
-                //printf("d_tot is %lli \n " ,d_total);
-                pi = (double)inside_tot / d_total * 4;
-                if (3.141590 <= pi && pi < 3.141599)
-                    done = 1;
-            }
-            MPI_Bcast(&done, 2, MPI_INT, 0, MPI_COMM_WORLD);
-        }while (done != 1);
         if (rank == 0) {
-            printf("Pi is %lf \n ", pi);
-            d_per_core = d_total / (long long int)size;
+            //printf("in_tot is %lli \n " ,inside_tot);
+            //printf("d_tot is %lli \n " ,d_total);
+            pi = (double)inside_tot / (double)d_total * 4;
+        }
+            MPI_Bcast(&done, 2, MPI_INT, 0, MPI_COMM_WORLD);
+        if (rank == 0) {
+            printf("Pi is %0.9f \n ", pi);
             printf("Total iterations %lli \n", d_total);
             printf("Iterations per core %lli \n", d_per_core);
             endtime = MPI_Wtime();
-            printf("That took %f seconds\n", endtime - starttime);
+            printf("The time taken is %f seconds\n", endtime - starttime);
         }
         MPI_Finalize();
         return 0;
